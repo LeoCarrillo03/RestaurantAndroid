bf2135db602e395623f10450f0c5140957683641
LeoCarrillo03 <CH74364901@ZEGELIPAE.PE>
2023-11-10 15:49:49 -0500

    Primera Actualizacion Del Restaurant

---
+++ app/src/main/java/com/example/myrestaurant/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myrestaurant
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button;
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent(this, PrincipalActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myrestaurant/PrincipalActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myrestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+
+}
